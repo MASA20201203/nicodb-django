df9164a369b7bd9a5398eb87f21e4b4efa37ccb9
MASA20201203 <koedame032@gmail.com>
2025-01-20 16:44:42 +0900

    setup: #1 toxを使えるように設定

--- 1_1_black.py
+++ 1_1_black.py
@@ -1,7 +1,5 @@
 import os
 
-import boto3
-
 
 def sum(a, b):
     """aとbを足す"""
@@ -10,5 +8,4 @@ def sum(a, b):
 
 def show_doc():
     """この関数のドキュメントを表示"""
-    print(boto3.__doc__)
     print(os.__doc__)
