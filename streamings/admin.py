7d98afd1a34c1c02e043060b65ce08a979e6a523
MASA20201203 <koedame032@gmail.com>
2025-01-30 15:47:13 +0900

    add: #5 管理画面に作成したモデルを追加

--- streamings/admin.py
+++ streamings/admin.py
@@ -1,3 +1,22 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from .models import Streamer, Streaming
+
+
+@admin.register(Streamer)
+class StreamerAdmin(admin.ModelAdmin):
+    list_display = ("id", "streamer_id", "name")
+
+
+@admin.register(Streaming)
+class StreamingAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "streaming_id",
+        "title",
+        "start_time",
+        "end_time",
+        "duration_time",
+        "status",
+        "streamer_id",
+    )
